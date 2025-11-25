043a2fcd8115ec5633b554ecbfa1d8d679a7d832
Pratikpatil43 <patilpratik4388@gmail.com>
2025-11-25 22:10:21 +0530

    Fixed frontend folders not uploading

--- Frontend Apps/masterAdmin_Frontend/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/App.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
